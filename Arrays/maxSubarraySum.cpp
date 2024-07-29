65f88a7fb6dbb16d0e99c05bb2572283826cf92f
Saurav7012 <132158411+Saurav7012@users.noreply.github.com>
2024-07-29 21:42:48 +0530

    Add files via upload

---
+++ Arrays/maxSubarraySum.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+
+    int i, n, arr[1000], Maxi=INT32_MIN;
+
+    cout<<"Enter the limit : ";
+    cin>>n;
+
+    for(i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+    int prefix=0;
+
+    for(i=0; i<n; i++)
+    {
+        prefix+=arr[i];
+        Maxi=max(Maxi,prefix);
+
+        if(prefix<0)
+        {
+            prefix=0;
+        }
+
+    }
+
+    cout<<"Maximum Subarray Sum : "<<Maxi;
+}
