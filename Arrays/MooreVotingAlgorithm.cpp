b0926ba69ac093a389d1a761e4f69217312a344e
Saurav7012 <132158411+Saurav7012@users.noreply.github.com>
2024-07-31 18:31:21 +0530

    Add files via upload

---
+++ Arrays/MooreVotingAlgorithm.cpp
@@ -0,0 +1,42 @@
+//Assume that there are occurrences greater than N/2
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+    int arr[100], i, N;
+
+    cout<<"Enter the limit : ";
+    cin>>N;
+
+    for(i=0; i<N; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int candidate, count=0;
+
+    for(i=0; i<N; i++)
+    {
+        if(count==0)
+        {
+            candidate=arr[i];
+            count=1;
+        }
+        else
+        {
+            if(arr[i]==candidate)
+            {
+                count++;
+            }
+            else
+            {
+                count--;
+            }
+        }
+    }
+
+    cout<<candidate<<" has occured the most times";
+}
