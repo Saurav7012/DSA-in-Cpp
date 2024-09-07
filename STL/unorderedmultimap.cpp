e3ade29d412fa2874fde1abfb6aff67b2a6586d0
Saurav7012 <132158411+Saurav7012@users.noreply.github.com>
2024-09-08 00:20:09 +0530

    Add files via upload

---
+++ STL/unorderedmultimap.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    unordered_multimap<int, int>m;
+
+    m.insert(make_pair(10, 100));
+    m.insert(make_pair(20, 200));
+    m.insert(make_pair(50, 500));
+    m.insert(make_pair(30, 300));
+    m.insert(make_pair(80, 800));
+    m.insert(make_pair(70, 700));
+    m.insert(make_pair(20, 1000));
+    m.insert(make_pair(90, 900));
+
+    for(auto it = m.begin(); it != m.end(); it++)
+    {
+        cout<<it -> first<<" "<<it -> second<<endl;
+    }
+}    
